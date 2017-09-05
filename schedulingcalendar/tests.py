968864cc8b6f186a09caf55286bd24de265a23b8
RyanKJ <ryankj42@gmail.com>
2017-09-05 11:26:04 -0500

    Testing part 3

--- schedulingcalendar/tests.py
+++ schedulingcalendar/tests.py
@@ -5,7 +5,7 @@ from .models import (Schedule, Department, DepartmentMembership, MonthlyRevenue,
                      Employee, Vacation, RepeatUnavailability, BusinessData,
                      Absence, DesiredTime, LiveSchedule, LiveCalendar)
 from .business_logic import get_availability
-from datetime import datetime, date, timedelta
+from datetime import datetime, date, time, timedelta
 
 
 def create_employee(user, first_name='A', last_name='1', email="a@a.com", 
@@ -38,6 +38,31 @@ def create_schedule(user, start_dt, end_dt, department, hide_start_time=False,
                                        department=department, employee=employee)      
 
     return schedule
+    
+    
+def create_vacation(user, employee, start_dt, end_dt):
+    """Creates a vacation with optional customization."""
+    vacation = Vacation.objects.create(user=user, start_datetime=start_dt, 
+                                       end_datetime=end_dt, employee=employee)      
+
+    return vacation
+ 
+
+def create_absence(user, employee, start_dt, end_dt):
+    """Creates an absence with optional customization."""
+    absence = Absence.objects.create(user=user, start_datetime=start_dt, 
+                                       end_datetime=end_dt, employee=employee)      
+
+    return absence
+ 
+    
+def create_unav_repeat(user, employee, start, end, weekday):
+    """Creates a repeating unavailability with optional customization."""
+    unav_repeat = RepeatUnavailability.objects.create(user=user, start_time=start, 
+                                                      end_time=end, weekday=weekday,
+                                                      employee=employee)      
+
+    return unav_repeat
                      
                      
 class GetAvailabilityTest(TestCase):
@@ -54,8 +79,8 @@ class GetAvailabilityTest(TestCase):
         business_data = create_business_data(user)
         department = create_department(user)                     
         employee = create_employee(user)
-        start_dt = datetime(2017, 1, 1, 0, 0, 0)
-        end_dt = datetime(2017, 1, 1, 1, 0, 0)
+        start_dt = datetime(2017, 1, 2, 0, 0, 0)
+        end_dt = datetime(2017, 1, 2, 1, 0, 0)
         schedule = create_schedule(user, start_dt=start_dt, end_dt=end_dt,
                                    department=department, employee=employee)
                                    
@@ -83,8 +108,8 @@ class GetAvailabilityTest(TestCase):
         schedule_conflict = Schedule.objects.first()
         
         # Make an overlapping schedule to assign employee to:
-        start_dt = datetime(2017, 1, 1, 0, 59, 59)
-        end_dt = datetime(2017, 1, 1, 1, 0, 1)
+        start_dt = datetime(2017, 1, 2, 0, 59, 59)
+        end_dt = datetime(2017, 1, 2, 1, 0, 1)
         schedule = create_schedule(user, start_dt=start_dt, end_dt=end_dt,
                                    department=department, employee=employee)
         availability = get_availability(employee, schedule)          
@@ -100,12 +125,70 @@ class GetAvailabilityTest(TestCase):
     
     def test_vacation_conflict(self):
         """Case where there is a vacation conflict in availability."""
-        pass
+        user = User.objects.first()
+        employee = Employee.objects.first()
+        schedule_conflict = Schedule.objects.first()
+        
+        # Make an overlapping schedule to assign employee to:
+        start_dt = datetime(2017, 1, 2, 0, 59, 59)
+        end_dt = datetime(2017, 1, 2, 1, 0, 1)
+        vacation = create_vacation(user, start_dt=start_dt, end_dt=end_dt,
+                                   employee=employee)
+        availability = get_availability(employee, schedule_conflict)          
+
+        self.assertEqual(list(availability['(S)']), [])
+        self.assertEqual(list(availability['(V)']), [vacation])
+        self.assertEqual(list(availability['(A)']), [])
+        self.assertEqual(list(availability['(U)']), [])
+        self.assertEqual(list(availability['Desired Times']), [])
+        self.assertEqual(availability['Hours Scheduled'], 2)
+        self.assertEqual(availability['(O)'], False)
+        
+        
+    def test_absence_conflict(self):
+        """Case where there is an absence conflict in availability."""
+        user = User.objects.first()
+        employee = Employee.objects.first()
+        schedule_conflict = Schedule.objects.first()
+        
+        # Make an overlapping schedule to assign employee to:
+        start_dt = datetime(2017, 1, 2, 0, 59, 59)
+        end_dt = datetime(2017, 1, 2, 1, 0, 1)
+        absence = create_absence(user, start_dt=start_dt, end_dt=end_dt,
+                                   employee=employee)
+        availability = get_availability(employee, schedule_conflict)          
+
+        self.assertEqual(list(availability['(S)']), [])
+        self.assertEqual(list(availability['(V)']), [])
+        self.assertEqual(list(availability['(A)']), [absence])
+        self.assertEqual(list(availability['(U)']), [])
+        self.assertEqual(list(availability['Desired Times']), [])
+        self.assertEqual(availability['Hours Scheduled'], 2)
+        self.assertEqual(availability['(O)'], False)
         
         
     def test_repeate_unav_conflict(self):
         """Case where there is a repeat unavilability conflict in availability."""
-        pass
+        user = User.objects.first()
+        employee = Employee.objects.first()
+        department = Department.objects.first()
+        schedule_conflict = Schedule.objects.first()
+        
+        # Make an overlapping schedule to assign employee to:
+        start = time(6, 59, 59) # Set to 6 for UTC offset of CST
+        end = time(6, 0, 1) 
+        weekday = 0
+        unav_repeat = create_unav_repeat(user, start=start, end=end, 
+                                         weekday=weekday, employee=employee)
+        availability = get_availability(employee, schedule_conflict)      
+ 
+        self.assertEqual(list(availability['(S)']), [])
+        self.assertEqual(list(availability['(V)']), [])
+        self.assertEqual(list(availability['(A)']), [])
+        self.assertEqual(list(availability['(U)']), [unav_repeat])
+        self.assertEqual(list(availability['Desired Times']), [])
+        self.assertEqual(availability['Hours Scheduled'], 2)
+        self.assertEqual(availability['(O)'], False)
         
         
     def test_overtime_conflict(self):
